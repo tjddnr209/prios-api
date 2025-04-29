0555b90b4c40fa7f9368e9a3ce47b684be357d30
FuryJoker <tjddnr209@naver.com>
2025-04-29 15:00:20 +0900

    초기 프로젝트 생성 및 기본 설정 추가

---
+++ src/test/java/com/official/prios_api/PriosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.official.prios_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
